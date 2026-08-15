52d68207fb84b650a977403b88caa8006d44a00b
mabamabam <pup010405@gmail.com>
2026-08-15 15:53:49 +0900

    Add new unsold image

--- js/function.js
+++ js/function.js
@@ -82,6 +82,7 @@ document.addEventListener("DOMContentLoaded", function(){
             {unsold: "images/blue/27.jpg"},
             {unsold: "images/blue/118.jpg"},
             {unsold: "images/blue/8.jpg"},
+            {unsold: "images/blue/139.webp"},
             {unsold: "images/blue/105.jpg"},
             {unsold: "images/blue/99.jpg"},
             {unsold: "images/white/113.jpg"},
