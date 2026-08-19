2b4940168a5531f08feea78a719039b65d19162b
mabamabam <pup010405@gmail.com>
2026-08-19 22:01:56 +0900

    Remove unsold image

--- js/function.js
+++ js/function.js
@@ -81,7 +81,6 @@ document.addEventListener("DOMContentLoaded", function(){
             {unsold: "images/blue/45.jpg"},
             {unsold: "images/white/136.webp"},
             {unsold: "images/blue/27.jpg"},
-            {unsold: "images/blue/118.jpg"},
             {unsold: "images/blue/8.jpg"},
             {unsold: "images/blue/139.webp"},
             {unsold: "images/blue/105.jpg"},
