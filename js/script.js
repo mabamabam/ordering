41d4d34ba7ea07e4538e1f8446eb6c59edb48932
mabamabam <pup010405@gmail.com>
2026-09-08 15:03:25 +0900

    Add new entry for '맹시' in script.js

--- js/script.js
+++ js/script.js
@@ -57,6 +57,7 @@
     // { title: "검푸른 장막 너머로", author: "IZE", spine: 20, color: "#222222", image: "archive(2)/1.webp", context: "커미션 | A5" },
     // { title: "Cognitive Error", author: "403", spine: 30, color: "#ddff4d", image: "archive(2)/48.webp", context: "레디메이드 | A5" },
     // { title: "망사랑이 딱 좋아!", author: "앤솔로지", spine: 30, color: "#222222", image: "archive(2)/1.webp", context: "커미션 | A5 | 목차 및 장표지 (B) | 문진 굿즈" },
+    { title: "맹시", author: "@", spine: 20, color: "#ddff4d", image: "archive(2)/54.webp", context: "레디메이드 | 웹소설 표지" },
     { title: "Afterimage", author: "403", spine: 20, color: "#ddff4d", image: "archive(2)/53.webp", context: "레디메이드 | A5 | 중철" },
     { title: "Cognitive Error", author: "403", spine: 30, color: "#ddff4d", image: "archive(2)/52.webp", context: "레디메이드 | A5" },
     { title: "The Last Variable", author: "사과", spine: 23, color: "#ddff4d", image: "archive(2)/51.webp", context: "레디메이드 | A5" },
